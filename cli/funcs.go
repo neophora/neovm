e98c0777cb4afee3ad79e1061566d74395c5c09c
yljjgroup <tianzhou.lu@jiajiagroup.net>
2020-12-04 18:27:41 +0800

    implement others

---
+++ cli/funcs.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"errors"
+	"github.com/nspcc-dev/neo-go/pkg/core/block"
+	"github.com/nspcc-dev/neo-go/pkg/core/transaction"
+	"github.com/nspcc-dev/neo-go/pkg/util"
+	"github.com/nspcc-dev/neo-go/pkg/vm"
+	"math"
+)
+
+func getBlockHashFromElement(element *vm.Element) (util.Uint256, error) {
+	var hash util.Uint256
+	hashbytes := element.Bytes()
+	if len(hashbytes) <= 5 {
+		hashint := element.BigInt().Int64()
+		if hashint < 0 || hashint > math.MaxUint32 {
+			return hash, errors.New("bad block index")
+		}
+		// TODO:
+		//hash = bc.GetHeaderHash(int(hashint)) call RPC GetHashByHeight
+	} else {
+		return util.Uint256DecodeBytesBE(hashbytes)
+	}
+	return hash, nil
+}
+
+func getTransactionAndHeight(v *vm.VM) (*transaction.Transaction, uint32, error) {
+	hashbytes := v.Estack().Pop().Bytes()
+	hash, err := util.Uint256DecodeBytesBE(hashbytes)
+	if err != nil {
+		return nil, 0, err
+	}
+	// TODO: call RPC to get transaction By hash;
+	// return cd.GetTransaction(hash)
+	return ...
+}
+
+func popHeaderFromVM(v *vm.VM) (*block.Header, error) {
+	iface := v.Estack().Pop().Value()
+	header, ok := iface.(*block.Header)
+	if !ok {
+		block, ok := iface.(*block.Block)
+		if !ok {
+			return nil, errors.New("value is not a header or block")
+		}
+		return block.Header(), nil
+	}
+	return header, nil
+}
+
+
--- cli/main.go
+++ cli/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/nspcc-dev/neo-go/pkg/core/block"
+	"github.com/nspcc-dev/neo-go/pkg/util"
+	"math"
 
 	"github.com/nspcc-dev/neo-go/pkg/vm"
+
 )
 
 func main() {
@@ -24,30 +29,153 @@ func main() {
 	v.RegisterInteropGetter(func(id uint32) *vm.InteropFuncPrice {
 		switch id {
 		case vm.InteropNameToID([]byte("System.Block.GetTransaction")):
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				blockInterface:=v.Estack().Pop().Value()
+				block, ok := blockInterface.(*block.Block)
+				if !ok {
+					return errors.New("value is not a block")
+				}
+			},
+		}
+			index := v.Estack().Pop().BigInt().Int64()
 			return nil
 		case vm.InteropNameToID([]byte("System.Block.GetTransaction")):
-			return nil
+			return &vm.InteropFuncPrice{
+				Func: func(v *vm.VM) error {
+					blockInterface := v.Estack().Pop().Value()
+					block, ok := blockInterface.(*block.Block)
+					if !ok {
+						return errors.New("value is not a block")
+					}
+					index := v.Estack().Pop().BigInt().Int64()
+					if index < 0 || index >= int64(len(block.Transactions)) {
+						return errors.New("wrong transaction index")
+					}
+					tx := block.Transactions[index]
+					v.Estack().PushVal(vm.NewInteropItem(tx))
+					return nil
+				},
+				Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Block.GetTransactionCount")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				blockInterface := v.Estack().Pop().Value()
+				block, ok := blockInterface.(*block.Block)
+				if !ok {
+					return errors.New("value is not a block")
+				}
+				v.Estack().PushVal(len(block.Transactions))
+				return nil
+			},
+			Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Block.GetTransactions")):
 			return nil
 		case vm.InteropNameToID([]byte("System.Blockchain.GetBlock")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				hash, err := getBlockHashFromElement(v.Estack().Pop())
+				if err != nil {
+					return err
+				}
+
+				// TODO: call RPC to get the block by Hash or Height
+				 block := Request(hash)
+				if err != nil {
+					v.Estack().PushVal([]byte{})
+				} else {
+					v.Estack().PushVal(vm.NewInteropItem(block))
+				}
+				return nil
+			},
+			Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Blockchain.GetContract")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				hashbytes := v.Estack().Pop().Bytes()
+				hash, err := util.Uint160DecodeBytesBE(hashbytes)
+				if err != nil {
+					return err
+				}
+				// TODO: call adhoccontractstate://hash-height/ here to fetch the contract State
+				// cs, err := ic.dao.GetContractState(hash)
+				if err != nil {
+					v.Estack().PushVal([]byte{})
+				} else {
+					v.Estack().PushVal(vm.NewInteropItem(cs))
+				}
+				return nil
+			},
+			Price:1,
+			}
 		case vm.InteropNameToID([]byte("System.Blockchain.GetHeader")):
-			return nil
+			return &vm.InteropFuncPrice{
+				Func: func(vm *vm.VM) error {
+					hash, err := getBlockHashFromElement(v.Estack().Pop())
+					if err != nil {
+						return err
+					}
+					// TODO: call RPC to get the block by hash or Height
+					// header, err := ic.bc.GetHeader(hash)
+					if err != nil {
+						v.Estack().PushVal([]byte{})
+					} else {
+						v.Estack().PushVal(vm.NewInteropItem(header))
+					}
+					return nil
+				},
+				Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Blockchain.GetHeight")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				// CALL RPC to get the height
+				blockchainHeight = ...
+				v.Estack().PushVal(blockchainHeight)
+				return nil
+			},
+			Price:1,
+			}
 		case vm.InteropNameToID([]byte("System.Blockchain.GetTransaction")):
-			return nil
+            return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				tx, _, err := getTransactionAndHeight(v)
+				if err != nil {
+					return err
+				}
+				v.Estack().PushVal(vm.NewInteropItem(tx))
+				return nil
+			},
+			Price:1,
+
+			}
 		case vm.InteropNameToID([]byte("System.Blockchain.GetTransactionHeight")):
-			return nil
+			//
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				_, h, err := getTransactionAndHeight(v)
+				if err != nil {
+					return err
+				}
+				v.Estack().PushVal(h)
+				return nil
+			},
+			Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Contract.Destroy")):
 			return nil
 		case vm.InteropNameToID([]byte("System.Contract.GetStorageContext")):
 			return nil
 		case vm.InteropNameToID([]byte("System.ExecutionEngine.GetCallingScriptHash")):
+			return &vm.InteropFuncPrice{
+				Func: func(v *vm.VM) error {
+					return vm.RuntimeSerialize(v)
+				},
+				Price: 1,
+			}
 			return nil
 		case vm.InteropNameToID([]byte("System.ExecutionEngine.GetEntryScriptHash")):
 			return nil
@@ -56,13 +184,53 @@ func main() {
 		case vm.InteropNameToID([]byte("System.ExecutionEngine.GetScriptContainer")):
 			return nil
 		case vm.InteropNameToID([]byte("System.Header.GetHash")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				header, err := popHeaderFromVM(v)
+				if err != nil {
+					return err
+				}
+				v.Estack().PushVal(header.Hash().BytesBE())
+				return nil
+			},
+			Price:1,
+			}
 		case vm.InteropNameToID([]byte("System.Header.GetIndex")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				header, err := popHeaderFromVM(v)
+				if err != nil {
+					return err
+				}
+				v.Estack().PushVal(header.Index)
+				return nil
+			},
+			Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Header.GetPrevHash")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				header, err := popHeaderFromVM(v)
+				if err != nil {
+					return err
+				}
+				v.Estack().PushVal(header.PrevHash.BytesBE())
+				return nil
+			},
+			Price: 1,
+			}
 		case vm.InteropNameToID([]byte("System.Header.GetTimestamp")):
-			return nil
+			return &vm.InteropFuncPrice{
+			Func: func(vm *vm.VM) error {
+				header, err := popHeaderFromVM(v)
+				if err != nil {
+					return err
+				}
+				v.Estack().PushVal(header.Timestamp)
+				return nil
+			},
+			Price:1,
+			}
 		case vm.InteropNameToID([]byte("System.Runtime.CheckWitness")):
 			return nil
 		case vm.InteropNameToID([]byte("System.Runtime.Deserialize")):
@@ -73,6 +241,18 @@ func main() {
 				Price: 1,
 			}
 		case vm.InteropNameToID([]byte("System.Runtime.GetTime")):
+			return &vm.InteropFuncPrice{
+				Func: func(v *vm.VM) error {
+					// User current
+					// we will get the header by call rpc
+					height, err := getCurrentBlock()
+					// header://height/{UINT64}
+					header, err := getHeader()
+					v.Estack().PushVal(header.Timestamp)
+					return nil
+				},
+				Price: 1,
+			}
 			return nil
 		case vm.InteropNameToID([]byte("System.Runtime.GetTrigger")):
 			return nil
@@ -97,27 +277,27 @@ func main() {
 		case vm.InteropNameToID([]byte("System.Storage.Get")):
 			return &vm.InteropFuncPrice{
 				Func: func(v *vm.VM) error {
-					// stcInterface := v.Estack().Pop().Value()
-					// stc, ok := stcInterface.(*StorageContext)
-					// if !ok {
-					// return fmt.Errorf("%T is not a StorageContext", stcInterface)
-					// }
-					// contract, err := GetContractState(stc.ScriptHash) // CALL RPC
-					// if err != nil {
-					// return errors.New("no contract found")
-					// }
-					// if !contract.HasStorage() {
-					// return fmt.Errorf("contract %s can't use storage", stc.ScriptHash)
-					// }
-					// key := v.Estack().Pop().Bytes()
-					// FIRST READ CACHE
-					// THEN RPC
-					// si := GetStorageItem(stc.ScriptHash, key) // CALL RPC
-					// if si != nil && si.Value != nil {
-					// v.Estack().PushVal(si.Value)
-					// } else {
-					// v.Estack().PushVal([]byte{})
-					// }
+					stcInterface := v.Estack().Pop().Value()
+					stc, ok := stcInterface.(*StorageContext)
+					if !ok {
+					return fmt.Errorf("%T is not a StorageContext", stcInterface)
+					}
+					contract, err := GetContractState(stc.ScriptHash) // CALL RPC
+					if err != nil {
+					return errors.New("no contract found")
+					}
+					if !contract.HasStorage() {
+					return fmt.Errorf("contract %s can't use storage", stc.ScriptHash)
+					}
+					key := v.Estack().Pop().Bytes()
+					FIRST READ CACHE
+					THEN RPC
+					si := GetStorageItem(stc.ScriptHash, key) // CALL RPC
+					if si != nil && si.Value != nil {
+					v.Estack().PushVal(si.Value)
+					} else {
+					v.Estack().PushVal([]byte{})
+					}
 					return nil
 				},
 				Price: 0, // TODO FIX
